d803a5699a8eaa56016e4239a40eb80046e7e219
Mriganka Dhar <mrigankodhar0001@gmail.com>
2023-04-12 23:11:35 +0530

    add cardinfo for all card details

---
+++ cardinfo.js
@@ -0,0 +1,33 @@
+// Define the card ranks, suits, and values
+const RANKS = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+const SUITS = ["C", "D", "H", "S"];
+const VALUES = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+module.exports = { RANKS, SUITS, VALUES };
