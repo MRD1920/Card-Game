2f65d6e9af72b0a31addb4cfd26d3010280a400f
Mriganka Dhar <mrigankodhar0001@gmail.com>
2023-04-13 12:30:50 +0530

    deck is complete

--- deck.js
+++ deck.js
@@ -1,6 +1,6 @@
 import card from "./card";
-import { RANKS, VALUES, SUITS } from "./cardinfo";
-const shuffle = require("lodash/shuffle");
+import { RANKS, SUITS } from "./cardinfo";
+//const shuffle = require("lodash/shuffle");
 
 export default class Deck {
   constructor() {
@@ -11,5 +11,28 @@ export default class Deck {
       }
     }
     this.cards = shuffle(this.cards);
+  }
+
+  shuffle(cards) {
+    let currentIndex = cards.length,
+      randomIdx;
+    while (currentIndex != 0) {
+      randomIdx = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // now swap them
+      [cards[currentIndex], cards[randomIdx]] = [
+        cards[randomIdx],
+        cards[currentIndex],
+      ];
+    }
+    return this.shuffle;
+  }
+
+  deal(numCards) {
+    const hand = [];
+    for (let i = 0; i < numCards; i++) {
+      hand.push(this.cards.pop());
+    }
+    return hand;
   }
 }
