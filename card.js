e21da2c5443d490dc59ba0cb54113608ecd9fff5
Mriganka Dhar <mrigankodhar0001@gmail.com>
2023-04-13 16:20:36 +0530

    card creates card for every value and suit of card

--- card.js
+++ card.js
@@ -1,9 +1,12 @@
 import { VALUES } from "./cardinfo";
 
-export default class card {
+export default class Card {
   constructor(rank, suit) {
     this.rank = rank;
     this.suit = suit;
     this.value = VALUES[rank];
+  }
+  toString() {
+    return this.rank + this.suit;
   }
 }
